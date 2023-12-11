1436d06dcd6928486361d829a8af2e5d8406168c
vaibhav singh <vaibhavsinghgusain99@gmail.com>
2023-12-11 15:33:51 +0530

    multer middleware created

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const DB_NAME = 'professionalbackend';
 //.env
 export const MONGODB_URL = 'mongodb://192.168.31.107:27017';
 export const PORT = 3000;
-export const ACCESS_TOKEN_SECRET = ;
-export const ACCESS_TOKEN_EXPIRY = 1d;
-export const REFRESH_TOKEN_SECRET = ;
-export const REFRESH_TOKEN_EXPIRY = 10d;
+export const ACCESS_TOKEN_SECRET ='wickedisgood';
+export const ACCESS_TOKEN_EXPIRY = '1d';
+export const REFRESH_TOKEN_SECRET ='wickedisgood';
+export const REFRESH_TOKEN_EXPIRY = '10d';
